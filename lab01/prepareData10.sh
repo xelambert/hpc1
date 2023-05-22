75a0934f99cfb3a791b3983e989c48cd498f9ef2
xelambert <xelambert@gmail.com>
2023-05-22 23:03:14 +0300

    all

--- lab01/prepareData10.sh
+++ lab01/prepareData10.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "-4088 1561 36" | awk '{xmin=$1; xmax=$2; steps=$3; dx=(xmax-xmin)/steps;x=xmin; for (i=0; i<steps; i++){print x" "x+dx;x+=dx}}' >solv.inp
+echo "-4088 1561 300" | awk '{xmin=$1; xmax=$2; steps=$3; dx=(xmax-xmin)/steps;x=xmin; for (i=0; i<steps; i++){print x" "x+dx;x+=dx}}' >solv.inp
